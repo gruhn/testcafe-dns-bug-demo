c595715fbf61729b613e01a25b2d9e068ddb37b5
Niklas Gruhn <niklas.gruhn@deepl.com>
2022-08-10 13:42:50 +0200

    adjust

--- test.spec.js
+++ test.spec.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe'
 
 fixture('Bug Demo')
-    .page("http://localhost:8080");
+    .page("https://gruhn.github.io/testcafe-dns-bug-demo/");
 
 for (let i = 1; i < 10; i++) {
     test(`Test ${i}`, async t => {
